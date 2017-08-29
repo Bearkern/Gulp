9d7857ac587b59d458da028d2a0c72950c4a7edc
Bearkern <haolin1224@gmail.com>
2017-08-29 20:12:09 +0800

    Update 2017-08-29T12:12:07.430Z

---
+++ js/all.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var newFn = function newFn() {
+  console.log('Hello');
+};
+
+newFn();
+
+// function newFn() {
+//   console.log('Hello');
+// }
+
+// newFn();
+'use strict';
+
+var newFn2 = function newFn2() {
+  console.log('b');
+};
+
+newFn2();
+
+// function newFn2() {
+//   console.log('Hello');
+// }
+
+// newFn2();
+//# sourceMappingURL=all.js.map
